014fb8d1d792e10a226ce77d0976c9f2e9265b63
EC2 Default User <ec2-user@ip-172-31-33-20.ap-northeast-1.compute.internal>
2019-01-23 08:14:52 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+const query = gql`
+  {
+    users (first: 10) {
+      nodes {
+        id
+        name
+        age 
+        score
+      }
+    }
   }
-}
+`;
+
+const App = () => (
+  <Query query={query}>
+    {({ loading, data }) => {
+      if (loading) return <p>Loading...</p>;
+      const users = data.users.nodes;
+      return (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              <p>名前:{user.name}</p>
+              <div>年齢:{user.age}</div>
+              <div>点数:{user.score}</div>
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://52.194.211.12:3000/graphql',
+  request: operation => {
+    operation.setContext({
+    });
+  }
+});
+
+const query = gql`
+  {
+    users {
+      edges {
+        node {
+          id
+          name
+          age
+          score
+        }
+      }
+    }
+  }
+`
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
